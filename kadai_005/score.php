a2197e73e6f10fa85d6eab724373899e41a1a42a
Taruso09 <r0i9o2a2@softbank.ne.jp>
2025-04-03 00:56:00 +0900

    課題提出

---
+++ kadai_005/score.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    $score = 80+60+55+40+100+25+80+95+30+60;
+
+    echo $score /10;
+    ?>
+  </p>
+</body>
+</html>
